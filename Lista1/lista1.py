93c6e99f0ec5433f41ca0dfa1f203a2f6638cbb0
Marcfr01 <gabriel_tf@live.com>
2026-03-31 17:03:28 -0300

    Mudanças na destruição de cada circuito

--- Lista1/lista1.py
+++ Lista1/lista1.py
@@ -17,16 +17,19 @@ cargas = [cte.Z1, cte.Z2, cte.Z3]
 casos = []
 
 # ============= Questão 1. ============= 
-'''
+
 for fonte, linha, carga in product(fontes, linhas, cargas):
     c = Circuito.questao_1(fonte= fonte, linha= linha, carga= carga, nome= f"G{linhas.index(linha) + 1}{cargas.index(carga) + 1}")
     casos.append(c)
 
 for i in casos:
     i.resolver_circuito()
-    #i.salvar_dados_q1("dados_lista1.txt")
+    i.salvar_dados_q1("dados_lista1.txt")
 
-for i in casos: del i # limpa o vetor casos'''
+for i in casos: 
+    del i # deleta cada caso
+
+casos.clear() # limpa o vetor de casos para a proxima questao
 
 # ============= Questão 2. =============
 #                                           ----- carga 1
@@ -61,41 +64,8 @@ fase2 = np.angle(circuito_III, deg=True)
 
 for i in casos:
     i.salvar_dados_q2("dados_lista1_q2")
-'''
-questao2 = pd.DataFrame(columns=["Circuito I", "Circuito II", "Circuito III"], index= ["Ia", "Ib", "Ic", "Vnn / In"])
-m0_flat = np.array(modulo0).flatten()
-f0_flat = np.array(fase0).flatten()
 
-m1_flat = np.array(modulo1).flatten()
-f1_flat = np.array(fase1).flatten()
+for i in casos:
+    del i # deleta cada caso
 
-m2_flat = np.array(modulo2).flatten()
-f2_flat = np.array(fase2).flatten()
-
-questao2["Circuito I"]   = [f"{m:.4f} ∠ {f:.2f}°" for m, f in zip(m0_flat, f0_flat)]
-questao2["Circuito II"]  = [f"{m:.4f} ∠ {f:.2f}°" for m, f in zip(m1_flat, f1_flat)]
-questao2["Circuito III"] = [f"{m:.4f} ∠ {f:.2f}°" for m, f in zip(m2_flat, f2_flat)]
-
-print("\n",questao2["Circuito I"])
-print("\n",questao2["Circuito II"])
-print("\n",questao2["Circuito III"])
-print()
-
-modulo0_l = np.abs(casos[0].get_tensoes_linha())
-fase0_l = np.angle(casos[0].get_tensoes_linha(), deg= True)
-print(f"Vaa':   {casos[0].get_tensoes_linha()[0]} = {modulo0_l[0][0]:.4f} ∠ {fase0_l[0][0]:.2f}°")
-print(f"Vbb':   {casos[0].get_tensoes_linha()[1]} = {modulo0_l[1][0]:.4f} ∠ {fase0_l[1][0]:.2f}°")
-print(f"Vcc':   {casos[0].get_tensoes_linha()[2]} = {modulo0_l[2][0]:.4f} ∠ {fase0_l[2][0]:.2f}°\n")
-
-modulo1_l = np.abs(casos[1].get_tensoes_linha())
-fase1_l = np.angle(casos[1].get_tensoes_linha(), deg= True)
-print(f"Vaa':   {casos[1].get_tensoes_linha()[0]} = {modulo1_l[0][0]:.4f} ∠ {fase1_l[0][0]:.2f}°")
-print(f"Vbb':   {casos[1].get_tensoes_linha()[1]} = {modulo1_l[1][0]:.4f} ∠ {fase1_l[1][0]:.2f}°")
-print(f"Vcc':   {casos[1].get_tensoes_linha()[2]} = {modulo1_l[2][0]:.4f} ∠ {fase1_l[2][0]:.2f}°\n")
-
-modulo2_l = np.abs(casos[2].get_tensoes_linha())
-fase2_l = np.angle(casos[2].get_tensoes_linha(), deg= True)
-print(f"Vaa':   {casos[2].get_tensoes_linha()[0]} = {modulo2_l[0][0]:.4f} ∠ {fase2_l[0][0]:.2f}°")
-print(f"Vbb':   {casos[2].get_tensoes_linha()[1]} = {modulo2_l[1][0]:.4f} ∠ {fase2_l[1][0]:.2f}°")
-print(f"Vcc':   {casos[2].get_tensoes_linha()[2]} = {modulo2_l[2][0]:.4f} ∠ {fase2_l[2][0]:.2f}°\n")
-'''
+casos.clear() # limpa o vetor de casos para a proxima questao
